3b0b6fdf73f06227d4bd555c60a551b49197c0f9
ZhuravlevDmitry <80485534+ZhuravlevDmitry@users.noreply.github.com>
2021-09-16 19:50:11 +0300

    do

---
+++ src/main/java/homeWork/work2.java
@@ -0,0 +1,19 @@
+package homeWork;
+
+public class work2 {
+
+
+
+    /**
+     * Hello world!
+     *
+     */
+    public static class App
+    {
+        public static void main( String[] args )
+        {
+            System.out.println( "Hello !" );
+        }
+    }
+
+        }
